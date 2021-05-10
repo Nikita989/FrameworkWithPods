189c6ce234b54d6f2b40e500ab1354300ac877e4
Nikita989 <nikita.hn@gmail.com>
2021-05-10 12:01:05 +0530

    First commit

---
+++ FrameworkWithPods/FrameworkWithPods/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  FrameworkWithPods
+//
+//  Created by Nikita on 10/05/21.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func doSomething(){
+        print("I made inside the framework")
+    }
+    
+    
+}
