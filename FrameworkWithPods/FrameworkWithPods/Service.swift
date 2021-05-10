dca7e6e2ac504af1b8ac094c96dd00042608de0a
Nikita989 <nikita.hn@gmail.com>
2021-05-10 15:24:16 +0530

    added some files

--- FrameworkWithPods/FrameworkWithPods/Service.swift
+++ FrameworkWithPods/FrameworkWithPods/Service.swift
@@ -7,15 +7,30 @@
 
 import Foundation
 import AFNetworking
+//import SDWebImage
 
 public class Service{
     
-    private init(){
-        
+    private init(){}
+    
+    public static let shared = Service()
+    
+    public var delegate:UIViewController = UIViewController()
+    
+    public  func doSomething(){
+        print("I was called from the framework")
     }
     
-    public static func doSomething(){
-        print("I made inside the framework")
+    public  func downloadImage(setDelegate:UIViewController){
+        self.delegate = setDelegate
+//        imageView.sd_setImage(with: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png" ), completed: nil)
+        let config = URLSessionConfiguration.default
+        let manager = AFURLSessionManager.init(sessionConfiguration: config)
+        let request = URLRequest.init(url: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png")!)
+       let task = manager.downloadTask(with: request, progress: nil, destination: nil) { response, url, error in
+//            <#code#>/
+        }
+        task.resume()
     }
     
     
