59e97261a9cf7aabfb53f17dada624c950702f59
Nikita989 <nikita.hn@gmail.com>
2021-05-10 16:00:21 +0530

    added

--- FrameworkWithPods/FrameworkWithPods/Service.swift
+++ FrameworkWithPods/FrameworkWithPods/Service.swift
@@ -9,36 +9,19 @@ import Foundation
 import AFNetworking
 import SDWebImage
 
-public protocol CommunicationProtocol{
-    
-    func imageDownloaded(response:URLResponse)
-}
-
 public class Service{
     
     private init(){}
     
     public static let shared = Service()
     
-    public var delegate:CommunicationProtocol!
     
     public  func doSomething(){
         print("I was called from the framework")
     }
     
     public  func downloadImage(imageView:UIImageView){
-//        self.delegate = setDelegate
         imageView.sd_setImage(with: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png" ), completed: nil)
-//        let config = URLSessionConfiguration.default
-//        let manager = AFURLSessionManager.init(sessionConfiguration: config)
-        let request = URLRequest.init(url: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png")!)
-//        let task = manager.downloadTask(with: request, progress: nil, destination: nil) { response, responseObject, error in
-//            print("responsee --->",response)
-//            print("error ---->",error)
-//            self.delegate.imageDownloaded(response: response)
-//        }
-//        task.resume()
-//        let requestOperation = AFHTTPReque/
     }
     
     
