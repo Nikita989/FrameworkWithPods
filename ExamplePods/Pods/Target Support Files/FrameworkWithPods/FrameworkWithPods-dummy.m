37ada37f7467d6b5c450ae32fa6f043856f6acb1
Nikita989 <nikita.hn@gmail.com>
2021-05-10 15:57:50 +0530

    changes

---
+++ ExamplePods/Pods/Target Support Files/FrameworkWithPods/FrameworkWithPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FrameworkWithPods : NSObject
+@end
+@implementation PodsDummy_FrameworkWithPods
+@end
--- FrameworkWithPods/FrameworkWithPods/Service.swift
+++ FrameworkWithPods/FrameworkWithPods/Service.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 import AFNetworking
-//import SDWebImage
+import SDWebImage
+
+public protocol CommunicationProtocol{
+    
+    func imageDownloaded(response:URLResponse)
+}
 
 public class Service{
     
@@ -15,22 +20,25 @@ public class Service{
     
     public static let shared = Service()
     
-    public var delegate:UIViewController = UIViewController()
+    public var delegate:CommunicationProtocol!
     
     public  func doSomething(){
         print("I was called from the framework")
     }
     
-    public  func downloadImage(setDelegate:UIViewController){
-        self.delegate = setDelegate
-//        imageView.sd_setImage(with: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png" ), completed: nil)
-        let config = URLSessionConfiguration.default
-        let manager = AFURLSessionManager.init(sessionConfiguration: config)
+    public  func downloadImage(imageView:UIImageView){
+//        self.delegate = setDelegate
+        imageView.sd_setImage(with: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png" ), completed: nil)
+//        let config = URLSessionConfiguration.default
+//        let manager = AFURLSessionManager.init(sessionConfiguration: config)
         let request = URLRequest.init(url: URL.init(string: "https://homepages.cae.wisc.edu/~ece533/images/fruits.png")!)
-       let task = manager.downloadTask(with: request, progress: nil, destination: nil) { response, url, error in
-//            <#code#>/
-        }
-        task.resume()
+//        let task = manager.downloadTask(with: request, progress: nil, destination: nil) { response, responseObject, error in
+//            print("responsee --->",response)
+//            print("error ---->",error)
+//            self.delegate.imageDownloaded(response: response)
+//        }
+//        task.resume()
+//        let requestOperation = AFHTTPReque/
     }
     
     
