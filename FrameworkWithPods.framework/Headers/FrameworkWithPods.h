b9650d139ae22f41786a4886fe021be5be4ca83f
Nikita989 <nikita.hn@gmail.com>
2021-05-10 19:45:37 +0530

    Zipped

---
+++ FrameworkWithPods.framework/Headers/FrameworkWithPods.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkWithPods.h
+//  FrameworkWithPods
+//
+//  Created by Nikita on 10/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkWithPods.
+FOUNDATION_EXPORT double FrameworkWithPodsVersionNumber;
+
+//! Project version string for FrameworkWithPods.
+FOUNDATION_EXPORT const unsigned char FrameworkWithPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkWithPods/PublicHeader.h>
+
+
